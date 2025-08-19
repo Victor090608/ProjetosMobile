4e2d162d13ef514994e2a617019a835458f359a0
VICTOR VIEIRA BARCELLOS <victor.5924@aluno.pr.senac.br>
2025-08-19 07:50:49 -0300

    montando a primeira tela inicial, colocando cores e arrumando estilizacao da tela

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,18 +8,23 @@ export default function login() {
         <View style={Style.container}>
             <View style={Style.boxTop}>
                 <Image
-                source={Logo}
+                    source={Logo}
+                    style={Style.logo}
+                    resizeMode="contain"
                 />
-                <Text>Bem vindo de volta!</Text>
+                <Text style={Style.text}>Bem vindo de volta</Text>
             </View>
             <View style={Style.boxMid}>
-                <Text>Endereço de E-mail</Text>
-                <TextInput />
-                <Text>Senha</Text>
+                <Text style={Style.titleInput}>ENDEREÇO DE E-MAIL</Text>
+                <View style={Style.boxInput}>
+                    <TextInput
+                        style={Style.input}
+                    />
+                </View>
+                <Text style={Style.titleInput}>SENHA</Text>
                 <TextInput />
             </View>
             <View style={Style.boxBottom}>
-                <Text>Bottom</Text>
             </View>
         </View>
     )
