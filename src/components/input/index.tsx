5a837151961776bedd722074782023d009add4e5
VICTOR VIEIRA BARCELLOS <victor.5924@aluno.pr.senac.br>
2025-09-01 14:46:40 -0300

    Atribuindo propriedades, componentes e biblioteca de icones, para acesso das diferentes plataformas

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, Fragment } from "react";
 import { View, Text, TextInput, TextInputProps } from "react-native";
-import { Style } from "../../pages/login/styles";
+import { style } from "./style";
 import { themas } from "../../global/themes";
 import { FontAwesome, MaterialIcons, Octicons } from '@expo/vector-icons';
 
@@ -24,12 +24,12 @@ type Props = TextInputProps & {
 
 export const Input = forwardRef(() => {
     return (
-        <>
-        <Text style={Style.titleInput}>ENDEREÇO DE E-MAIL</Text>
-        <View style={Style.boxInput}>
-            <TextInput style={Style.input}/>
+        <Fragment>
+        <Text style={style.titleInput}>ENDEREÇO DE E-MAIL</Text>
+        <View style={style.boxInput}>
+            <TextInput style={style.input}/>
             <MaterialIcons name="email" size={20} color={themas.colors.gray}/>
         </View>
-        </>
+        </Fragment>
     )
 })
