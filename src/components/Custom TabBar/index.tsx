9218d5973561d1f0e8f941e1879ca171992241ba
VICTOR VIEIRA BARCELLOS <victor.5924@aluno.pr.senac.br>
2025-09-15 10:16:57 -0300

    Criando area no bottom, deixando mais organizado para aplicar o botao na segunda tela

--- src/components/Custom TabBar/index.tsx
+++ src/components/Custom TabBar/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Touchable, TouchableOpacity } from "react-native";
 import { style } from "./styles";
 
 export default ({ state, navigation }: any) => {
     return (
         <Text>
-            Olá, Mundo!
+            <TouchableOpacity>
+                <Text>Esquerda</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>Centro</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>Direita</Text>
+            </TouchableOpacity>
         </Text>
     )
 }
