403e6f3d6915ce3915e0be3732f4516181aff679
VICTOR VIEIRA BARCELLOS <victor.5924@aluno.pr.senac.br>
2025-09-17 08:52:54 -0300

    Criando AuthContext, aplicando modal nas duas telas de list e user

--- src/components/Custom TabBar/index.tsx
+++ src/components/Custom TabBar/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, Touchable, TouchableOpacity, View } from "react-native";
 import { style } from "./styles";
 import { AntDesign, Entypo, FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import { themas } from "../../global/themes";
+import { AuthContextList } from "../../context/authContext_list";
 
 export default ({ state, navigation }: any) => {
+    const { onOpen } = useContext<any>(AuthContextList)
+
     const go = (screenName: string) => {
         navigation.navigate(screenName)
     }
@@ -13,13 +16,14 @@ export default ({ state, navigation }: any) => {
             <TouchableOpacity style={style.tabItem} onPress={() => go("List")}>
                 <AntDesign
                     name="bars"
-                    style={{ opacity: state.index === 1 ? 1: 0.3,
+                    style={{
+                        opacity: state.index === 1 ? 1 : 0.3,
                         color: themas.colors.primary,
                         fontSize: 32
-                     }}
+                    }}
                 />
             </TouchableOpacity>
-            <TouchableOpacity style={style.tabItemButton}>
+            <TouchableOpacity style={style.tabItemButton} onPress={() => onOpen()}>
                 <View style={{ width: '100%', left: 10, top: 4 }}>
                     <Entypo
                         name="plus"
@@ -38,10 +42,11 @@ export default ({ state, navigation }: any) => {
             <TouchableOpacity style={style.tabItem} onPress={() => go("User")}>
                 <FontAwesome
                     name="user"
-                    style={{ opacity: state.index === 1 ? 1 : 0.3,
+                    style={{
+                        opacity: state.index === 1 ? 1 : 0.3,
                         color: themas.colors.primary,
                         fontSize: 32
-                     }}
+                    }}
                 />
             </TouchableOpacity>
         </View>
