bf285e3141c8f07882e2dd805567e106b833b66e
VICTOR VIEIRA BARCELLOS <victor.5924@aluno.pr.senac.br>
2025-09-24 08:56:21 -0300

    Criando flags no modal, atribuindo a renderizacao na constante para exibir as flags

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -3,8 +3,15 @@ import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from "react-nati
 import { MaterialIcons, AntDesign } from '@expo/vector-icons'
 import { Modalize } from "react-native-modalize";
 import { Input } from "../components/input";
+import { themas } from "../global/themes";
+import { Flag } from "../components/Flag";
 
 export const AuthContextList: any = createContext({});
+
+const flags = [
+    { caption: 'Urgente', color: themas.colors.red },
+    { caption: 'Opcional', color: themas.colors.blueLight }
+]
 
 export const AuthProviderList = (props: any): any => {
 
@@ -17,6 +24,20 @@ export const AuthProviderList = (props: any): any => {
     useEffect(() => {
         onOpen()
     }, [])
+
+    const _renderFlags = () => {
+        return (
+            flags.map((item, index) => (
+                <TouchableOpacity key={index}>
+                    <Flag
+                        caption={item.caption}
+                        color={item.color}
+                    />
+                </TouchableOpacity>
+            ))
+        )
+    }
+
     const _container = () => {
         return (
             <View style={styles.container}>
@@ -56,8 +77,8 @@ export const AuthProviderList = (props: any): any => {
                 </View>
                 <View style={styles.containerFlag}>
                     <Text style={styles.label}>Flags:</Text>
-                    <View style={{}}>
-
+                    <View style={styles.rowFlags}>
+                        {_renderFlags()}
                     </View>
                 </View>
             </View>
@@ -107,5 +128,10 @@ export const styles = StyleSheet.create({
     containerFlag: {
         width: '100%',
         padding: 10
+    },
+    rowFlags: {
+        flexDirection: 'row',
+        gap: 10,
+        marginTop: 10
     }
 })
