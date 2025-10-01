b53a0394b1c70345f6a7d55db04f84ccf6b576d7
VICTOR VIEIRA BARCELLOS <victor.5924@aluno.pr.senac.br>
2025-10-01 08:57:02 -0300

    Aplicando JSON no banco de dados e salvando no terminal, colocando funções alerta para atencao do usuario

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -1,11 +1,12 @@
 import React, { createContext, useContext, useRef, useEffect, useState } from "react";
-import { Dimensions, KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Alert, Dimensions, KeyboardAvoidingView, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { MaterialIcons, AntDesign } from '@expo/vector-icons'
 import { Modalize } from "react-native-modalize";
 import { Input } from "../components/input";
 import { themas } from "../global/themes";
 import { Flag } from "../components/Flag";
 import CustomDateTimePicker from "../components/CustomDateTimePicker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContextList: any = createContext({});
 
@@ -24,7 +25,7 @@ export const AuthProviderList = (props: any): any => {
     const [selectedTime, setSelectedTime] = useState(new Date());
     const [showDatePicker, setShowDatePicker] = useState(false);
     const [showTimePicker, setShowTimePicker] = useState(false);
-    const [item, setItem] = useState (0);
+    const [item, setItem] = useState(0);
 
     const onOpen = () => {
         modalizeRef?.current?.open();
@@ -37,9 +38,9 @@ export const AuthProviderList = (props: any): any => {
         return (
             flags.map((item, index) => (
                 <TouchableOpacity key={index}
-                onPress={() => {
-                    setSelectedFlag(item.caption)
-                }}
+                    onPress={() => {
+                        setSelectedFlag(item.caption)
+                    }}
                 >
                     <Flag
                         caption={item.caption}
@@ -56,21 +57,32 @@ export const AuthProviderList = (props: any): any => {
     const handleTimeChange = (date) => {
         setSelectedTime(date);
     }
-    const handleSave = () => {
-        const newItem = {
-            item: Date.now(),
-            title,
-            description,
-            flags: selectedFlag,
-            timeLimite: new Date (
-                selectedDate.getFullYear(),
-                selectedDate.getMonth(),
-                selectedDate.getDate(),
-                selectedTime.getHours(),
-                selectedTime.getMinutes()
-            ).toISOString(),
+    const handleSave = async () => {
+        if (!title || !description || !selectedFlag) {
+            return Alert.alert('Atenção', 'Preencha os campso corretamente!')
         }
+        try {
+            const newItem = {
+                item: Date.now(),
+                title,
+                description,
+                flags: selectedFlag,
+                timeLimite: new Date(
+                    selectedDate.getFullYear(),
+                    selectedDate.getMonth(),
+                    selectedDate.getDate(),
+                    selectedTime.getHours(),
+                    selectedTime.getMinutes()
+                ).toISOString(),
+            }
+            await AsyncStorage.setItem('tasklist', JSON.stringify(newItem))
+
+        } catch (error) {
+            console.log("Erro ao salvar o item", error)
+        }
+
     }
+
 
     const _container = () => {
         return (
@@ -78,98 +90,98 @@ export const AuthProviderList = (props: any): any => {
                 style={styles.container}
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             >
-                    <View style={styles.header}>
-                        <TouchableOpacity onPress={() => onClose()}>
-                            <MaterialIcons
-                                name="close"
-                                size={30}
-                            />
-                        </TouchableOpacity>
-                        <Text style={styles.title}>Criar Tarefa</Text>
-                        <TouchableOpacity onPress={() => handleSave()}>
-                            <AntDesign
-                                name="check"
-                                size={30}
-                            />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.content}>
-                        <Input
-                            title="Titulo"
-                            labelStyle={styles.label}
-                            value={title}
-                            onChangeText={setTitle}
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={() => onClose()}>
+                        <MaterialIcons
+                            name="close"
+                            size={30}
                         />
-                        <Input
-                            title="Descrição"
-                            labelStyle={styles.label}
-                            height={100}
-                            multiline
-                            numberOfLines={5}
-                            value={description}
-                            onChangeText={setDescription}
-                            textAlignVertical="top"
+                    </TouchableOpacity>
+                    <Text style={styles.title}>Criar Tarefa</Text>
+                    <TouchableOpacity onPress={() => handleSave()}>
+                        <AntDesign
+                            name="check"
+                            size={30}
                         />
-                    </View>
-                    <View style={{ width: '40%' }}>
-                        {/* <Input
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.content}>
+                    <Input
+                        title="Titulo"
+                        labelStyle={styles.label}
+                        value={title}
+                        onChangeText={setTitle}
+                    />
+                    <Input
+                        title="Descrição"
+                        labelStyle={styles.label}
+                        height={100}
+                        multiline
+                        numberOfLines={5}
+                        value={description}
+                        onChangeText={setDescription}
+                        textAlignVertical="top"
+                    />
+                </View>
+                <View style={{ width: '40%' }}>
+                    {/* <Input
                             title="Tempo limite:"
                             labelStyle={styles.label}
                         /> */}
-                        <View style={{ flexDirection: 'row', gap: 10, width: '100%' }}>
-                            <TouchableOpacity onPress={() => setShowDatePicker(true)} style={{width: 200}}>
-                                <Input
-                                    title="Data Limite"
-                                    labelStyle={styles.label}
-                                    editable={false}
-                                    value={selectedDate.toLocaleDateString()}
-                                    onPress={() => setShowDatePicker(true)}
-                                />
-                            </TouchableOpacity>
-                            <TouchableOpacity style={{width: 120}} onPress={() => setShowTimePicker(true)}>
-                                <Input
-                                    title="Hora Limite"
-                                    labelStyle={styles.label}
-                                    editable={false}
-                                    value={selectedTime.toLocaleTimeString()}
-                                    onPress={() => setShowDatePicker(true)}
-                                />
-                            </TouchableOpacity>
-                        </View>
-                        <CustomDateTimePicker
-                            onDateChange={handleDateChange}
-                            setShow={setShowDatePicker}
-                            show={showDatePicker}
-                            type={'date'}
-                        />
-                        <CustomDateTimePicker
-                            onDateChange={handleTimeChange}
-                            setShow={setShowTimePicker}
-                            show={showTimePicker}
-                            type={'time'}
-                        />
+                    <View style={{ flexDirection: 'row', gap: 10, width: '100%' }}>
+                        <TouchableOpacity onPress={() => setShowDatePicker(true)} style={{ width: 200 }}>
+                            <Input
+                                title="Data Limite"
+                                labelStyle={styles.label}
+                                editable={false}
+                                value={selectedDate.toLocaleDateString()}
+                                onPress={() => setShowDatePicker(true)}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{ width: 120 }} onPress={() => setShowTimePicker(true)}>
+                            <Input
+                                title="Hora Limite"
+                                labelStyle={styles.label}
+                                editable={false}
+                                value={selectedTime.toLocaleTimeString()}
+                                onPress={() => setShowDatePicker(true)}
+                            />
+                        </TouchableOpacity>
                     </View>
-                    <View style={styles.containerFlag}>
-                        <Text style={styles.label}>Flags:</Text>
-                        <View style={styles.rowFlags}>
-                            {_renderFlags()}
-                        </View>
+                    <CustomDateTimePicker
+                        onDateChange={handleDateChange}
+                        setShow={setShowDatePicker}
+                        show={showDatePicker}
+                        type={'date'}
+                    />
+                    <CustomDateTimePicker
+                        onDateChange={handleTimeChange}
+                        setShow={setShowTimePicker}
+                        show={showTimePicker}
+                        type={'time'}
+                    />
+                </View>
+                <View style={styles.containerFlag}>
+                    <Text style={styles.label}>Flags:</Text>
+                    <View style={styles.rowFlags}>
+                        {_renderFlags()}
                     </View>
+                </View>
             </KeyboardAvoidingView >
         )
     }
-return (
-    <AuthContextList.Provider value={{ onOpen }}>
-        {props.children}
-        <Modalize
-            ref={modalizeRef}
-            // modalHeight={Dimensions.get('window').height / 1.3}
-            childrenStyle={{ height: Dimensions.get('window').height / 1.3 }}
-        >
-            {_container()}
-        </Modalize>
-    </AuthContextList.Provider>
-)
+    return (
+        <AuthContextList.Provider value={{ onOpen }}>
+            {props.children}
+            <Modalize
+                ref={modalizeRef}
+                // modalHeight={Dimensions.get('window').height / 1.3}
+                childrenStyle={{ height: Dimensions.get('window').height / 1.3 }}
+            >
+                {_container()}
+            </Modalize>
+        </AuthContextList.Provider>
+    )
 }
 
 export const useAuth = () => useContext(AuthContextList);
