c3140c43e7d0e976e4e480c563dedc5d80ea6bab
VICTOR VIEIRA BARCELLOS <victor.5924@aluno.pr.senac.br>
2025-10-08 09:10:53 -0300

    Finalizando o projeto, fazendo uma tela simples de User e dessa maneira encerrando o App

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-
-import { Text, View } from "react-native";
+import { style } from "./styles";
+import { useNavigation, NavigationProp } from "@react-navigation/native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
+import Login from "../login";
+import { Ionicons } from "@expo/vector-icons";
+import { themas } from "../../global/themes";
 
 export default function User() {
+    const navigation = useNavigation<NavigationProp<any>>();
+
+    const handleLogout = () => {
+        Alert.alert("", "Você saiu da conta!");
+        return navigation.reset({ routes: [{ name: 'Login' }] })
+    };
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Lista</Text>
+        <View style={style.container}>
+            <Text style={style.name}>NOME DO USUÁRIO</Text>
+            <TouchableOpacity style={style.logoutButton} onPress={handleLogout}>
+                <Ionicons
+                    name="exit"
+                    color={themas.colors.gray}
+                    size={40}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
